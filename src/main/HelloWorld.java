59d4002cd71f24ce2dc6aaab5a4d6e06167c2ab0
Collin Jones <collin.g.jones34@gmail.com>
2022-03-01 09:08:31 -0600

    exercise from yesterday, forgot to push

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -1,6 +1,54 @@
 package main;
+
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello, World!");
+        System.out.print("Hello, World! \n");
+
+        int myFavoriteNumber;
+
+        myFavoriteNumber = 7;
+        System.out.println("My favorite number is : " + myFavoriteNumber);
+
+        char charToAdd1 = '?';
+
+        String myString;
+        myString = "What am I doing ";
+        System.out.println(myString + "with my life" + charToAdd1);
+
+//         long myNumber = 123;
+        float myNumber = (float) 3.14;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//
+//        int x = 4;
+//        x += 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(x);
+        System.out.println(y);
     }
 }
+
+
+
+
