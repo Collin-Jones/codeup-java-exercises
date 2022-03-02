f15a27f613669dc6d8fa6e926b66cccefa3a0681
Collin Jones <collin.g.jones34@gmail.com>
2022-03-01 23:44:45 -0600

    step two of the exercise

--- src/main/ControlFlowExercises.java
+++ src/main/ControlFlowExercises.java
@@ -1,11 +1,19 @@
 package main;
 
 public class ControlFlowExercises {
-    public static void main(String[] args){
-        int aNumber= 5;
-        while(aNumber<=15){
-            System.out.print(aNumber + " ");
-            aNumber++;
-        }
+    public static void main(String[] args) {
+//        int aNumber= 5;
+//        while(aNumber<=15){
+//            System.out.print(aNumber + " ");
+//            aNumber++;
+//        }
+//        long aNumber2 = 2;
+//
+//        do {
+//            System.out.print(aNumber2);
+//            aNumber2*=aNumber2;
+//        } while (aNumber2 < 1000000L);
+
+
     }
 }
