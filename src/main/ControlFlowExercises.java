ed9ebd5c5bcba0670f7b8dd0d28599f44a749033
Collin Jones <collin.g.jones34@gmail.com>
2022-03-01 22:54:14 -0600

    step one of the exercise

---
+++ src/main/ControlFlowExercises.java
@@ -0,0 +1,11 @@
+package main;
+
+public class ControlFlowExercises {
+    public static void main(String[] args){
+        int aNumber= 5;
+        while(aNumber<=15){
+            System.out.print(aNumber + " ");
+            aNumber++;
+        }
+    }
+}
