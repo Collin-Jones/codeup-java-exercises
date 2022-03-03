f8e20843db6668b2aeb61bfc1ae89965d242d010
Collin Jones <collin.g.jones34@gmail.com>
2022-03-03 10:23:52 -0600

    String exercise

--- src/main/StringExercise.java
+++ src/main/StringExercise.java
@@ -2,6 +2,17 @@ package main;
 
 public class StringExercise {
     public static void main(String[] args){
+        String pinkFloyd = "We don't need no education,  We don't need no thought control";
+        System.out.println(pinkFloyd);
 
+        String theS = "Check \"this \"out!, \"s inside of \"s!";
+        System.out.println(theS);
+
+        String windows = "In windows, the main drive is usually C:\\";
+        System.out.println(windows);
+
+        String backSlashes = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(backSlashes);
     }
 }
