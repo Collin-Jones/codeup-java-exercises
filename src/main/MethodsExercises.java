3d79ca9d1bdb0c7311f2618d81b4102708cb347c
Collin Jones <collin.g.jones34@gmail.com>
2022-03-04 12:13:58 -0600

    doing part 3 of the exercise

--- src/main/MethodsExercises.java
+++ src/main/MethodsExercises.java
@@ -29,4 +29,34 @@ public class MethodsExercises {
     private static int modulus(int num1, int num2) {
         return num1 % num2;
     }
+
+
+    public static int getInteger(int min, int max) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a number between 1 and 10: ");
+        int userInput = Integer.parseInt(scan.nextLine());
+
+        if(userInput < min){
+            System.out.println("Number is invalid.");
+            return getInteger(min, max);
+        }else if(userInput > max){
+            System.out.println("Number is invalid.");
+            return getInteger(min, max);
+        }else {
+            System.out.println("Number is valid.");
+            return userInput;
+        }
+    }
+
+    public static void factorial(){
+        int userInput = getInteger(1, 10);
+        long total = 1L;
+        System.out.println("You have entered " + userInput);
+        for (int fact = userInput; fact > 0; fact--) {
+            total = total * fact;
+        }
+    }
+
 }
+
+
