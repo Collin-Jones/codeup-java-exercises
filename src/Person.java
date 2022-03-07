eeb4a996b22be4a529b2d7d84311388babdc5f04
Collin Jones <collin.g.jones34@gmail.com>
2022-03-07 10:30:57 -0600

    did the first part of the exercise

---
+++ src/Person.java
@@ -0,0 +1,31 @@
+public class Person {
+    private String name;
+
+    public Person(String name) {
+
+    }
+
+    public String getName(){
+//TODO: return the person's name
+        return this.name;
+    }
+
+    public void setName(String name){
+//TODO: change the name field to the passed value
+        this.name = name;
+    }
+    public void sayHello(){
+//TODO: print a message to the console using the person's name
+        System.out.println(getName() + " says Hello");
+    }
+
+    public static void main(String[] args){
+        Person person1 = new Person("Collin");
+        System.out.println(person1.getName());
+        System.out.println(person1.getName());
+        person1.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person1.getName());
+
+    }
+}
