fafad8ff982a728ac2c8cd46e1d3c39770788c81
Collin Jones <collin.g.jones34@gmail.com>
2022-03-07 18:43:21 -0600

    doing a an exercise

--- src/main/Practice.java
+++ src/main/Practice.java
@@ -1,45 +1,42 @@
 package main;
-//Write a static function called mapValueToColor that takes an int parameter called colorVal and returns a string that is the associated color.
-// If no color maps to the given colorVal then return an empty string. The color table is as follows:
-//        1	Blue
-//        2 Red
-//        3	Orange
-//        4	Yellow
-//        5 Violet
-//        6	Black
-//        7	White
-//        example:
-//        String myColor = MondayWork.mapValueToColor(3);
-//        System.out.println(myColor); // prints “Orange”
-public class Practice {
-    public static void mapValueToColor() {
-        int colorVal = 1;
 
-        switch(colorVal){
-            case 1 :
-                System.out.println("Blue");
-                break;
-            case 2 :
-                System.out.println("Red");
-                break;
-            case 3 :
-                System.out.println("Orange");
-                break;
-            case 4 :
-                System.out.println("Yellow");
-                break;
-            case 5 :
-                System.out.println("Violet");
-                break;
-            case 6 :
-                System.out.println("Black");
-                break;
-            case 7 :
-                System.out.println("White");
-                break;
-            default :
-                System.out.println("");
-                break;
-        }
+public class Practice {
+    public static void main(String[] args) {
+        String myColor = Practice.mapValueToColor(1);
+        System.out.println(myColor);
     }
+
+        private static String mapValueToColor(int colorVal){
+
+            switch(colorVal){
+                case 1 :
+                    System.out.println("1: Blue");
+
+                case 2 :
+                    System.out.println("2: Red");
+
+                case 3 :
+                    System.out.println("3: Orange");
+
+                case 4 :
+                    System.out.println("4: Yellow");
+
+                case 5 :
+                    System.out.println("5: Violet");
+
+                case 6 :
+                    System.out.println("6: Black");
+
+                case 7 :
+                    System.out.println("7: White");
+
+                default :
+                    System.out.println("");
+
+
+            }
+            return "";
+        }
+
+
 }
