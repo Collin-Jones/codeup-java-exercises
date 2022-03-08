a97b8b311f29693db0ade2de9f97be2b60378e1d
Collin Jones <collin.g.jones34@gmail.com>
2022-03-07 19:02:17 -0600

    did part of the exercise Doc Rob gave me. Having some personal issues. Will finish it later

--- src/Studies/java/Practice.java
+++ src/Studies/java/Practice.java
@@ -1,4 +1,4 @@
-package main;
+package Studies.java;
 
 public class Practice {
     public static void main(String[] args) {
---
+++ src/Studies/java/spaceships.java
@@ -0,0 +1,13 @@
+package Studies.java;
+
+public class spaceships {
+    private String theShipName;
+    private int hull, shields, armor, speed;
+
+    public spaceships(String theShipName){
+        this.theShipName = theShipName;
+    }
+    public String getTheShipName(){
+        return theShipName;
+    }
+}
