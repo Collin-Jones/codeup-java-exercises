81bad07af60b680b10955536a43b260ae46dccd7
Collin Jones <collin.g.jones34@gmail.com>
2022-03-11 14:12:45 -0600

    did the first part of the exercise

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -12,4 +12,25 @@ public class Student {
         this.grades = new ArrayList<>();
     }
 
+    public String getName() {
+        return this.name;
+    }
+    public void addGrades(double grade){
+        this.grades.add(grade);
+    }
+    public double getGradeAverage(){
+        double sum = 0;
+        for (Double grade : this.grades){
+            sum +=grade;
+        }
+        return (sum / this.grades.size());
+    }
+
+    public static void main(String[] args) {
+        Student student = new Student("Collin");
+        student.addGrades(90);
+        student.addGrades(75);
+        student.addGrades(89);
+        System.out.println(student.getName() + "'s average is: " + student.getGradeAverage());
+    }
 }
