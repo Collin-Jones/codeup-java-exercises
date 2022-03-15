5de046e5ef96a93f64bb70f39bf3b89cc6811b52
Collin Jones <collin.g.jones34@gmail.com>
2022-03-15 11:54:28 -0500

    exception exercise

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -2,11 +2,11 @@ package shapes;
 
 import util.Input;
 
-public class CircleApp {
-    public static void main(String[] args){
-        Input input = new Input();
-        Circle newCircle = new Circle(input.getDouble());
-        System.out.println("Area: " + newCircle.getArea());
-        System.out.println("Circumference: " + newCircle.getCircumference());
-    }
-}
+//public class CircleApp {
+//    public static void main(String[] args){
+//        Input input = new Input();
+//        Circle newCircle = new Circle(input.getDouble());
+//        System.out.println("Area: " + newCircle.getArea());
+//        System.out.println("Circumference: " + newCircle.getCircumference());
+//    }
+//}
